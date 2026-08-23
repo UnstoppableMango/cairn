8d0b17d3ca6501abb130d4d02b619f908f374413
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-08-23 00:23:10 -0500

    ocd

--- clan.nix
+++ clan.nix
@@ -5,27 +5,29 @@
 }:
 let
   cairnLib = self.lib;
+
+  inherit (lib.modules) importApply;
 in
 {
   meta.name = "cairn";
 
   modules."@UnstoppableMango/pki" = import ./modules/service/pki;
-  modules."@UnstoppableMango/etcd" = lib.modules.importApply ./modules/service/etcd {
+  modules."@UnstoppableMango/etcd" = importApply ./modules/service/etcd {
     inherit cairnLib;
   };
-  modules."@UnstoppableMango/apiserver" = lib.modules.importApply ./modules/service/apiserver {
+  modules."@UnstoppableMango/apiserver" = importApply ./modules/service/apiserver {
     inherit cairnLib;
   };
-  modules."@UnstoppableMango/kubelet" = lib.modules.importApply ./modules/service/kubelet {
+  modules."@UnstoppableMango/kubelet" = importApply ./modules/service/kubelet {
     inherit cairnLib;
   };
-  modules."@UnstoppableMango/loadbalancer" = lib.modules.importApply ./modules/service/loadbalancer {
+  modules."@UnstoppableMango/loadbalancer" = importApply ./modules/service/loadbalancer {
     inherit cairnLib;
   };
-  modules."@UnstoppableMango/network" = lib.modules.importApply ./modules/service/network {
+  modules."@UnstoppableMango/network" = importApply ./modules/service/network {
     inherit cairnLib;
   };
-  modules."@UnstoppableMango/kubeconfig" = lib.modules.importApply ./modules/service/kubeconfig {
+  modules."@UnstoppableMango/kubeconfig" = importApply ./modules/service/kubeconfig {
     inherit cairnLib;
   };
   # self.inputs (rather than an `inputs` specialArg) so this resolves the
@@ -33,11 +35,11 @@ in
   # nixosTest driver's per-machine module composition re-imports this file
   # with only NixOS's standard specialArgs (self, lib, pkgs, ...), not
   # flake.nix's custom `inputs` override.
-  modules."@UnstoppableMango/inoculant" = lib.modules.importApply ./modules/service/inoculant {
+  modules."@UnstoppableMango/inoculant" = importApply ./modules/service/inoculant {
     inherit (self.inputs) inoculant;
   };
   modules."@UnstoppableMango/coredns" = import ./modules/service/coredns;
-  modules."@UnstoppableMango/flux" = lib.modules.importApply ./modules/service/flux {
+  modules."@UnstoppableMango/flux" = importApply ./modules/service/flux {
     inherit (self.inputs) a2b;
   };
 }
